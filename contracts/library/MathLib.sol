661cad3bf0fe0dfa2884b4ff0f36166c828dc461
0xKoiner <alexgray.bcn.esp@gmail.com>
2023-02-16 11:15:00 +0100

    feat: add compound interest calculation to MathLib

--- contracts/library/MathLib.sol
+++ contracts/library/MathLib.sol
@@ -22,6 +22,26 @@ library MathLib {
         return (a * 1e18 + b / 2) / b;
     }
 
+    /// @notice Compound interest via binomial approximation
+    function calculateCompoundInterest(
+        uint256 _rate,
+        uint256 _lastTimestamp,
+        uint256 _now
+    ) internal pure returns (uint256) {
+        uint256 exp = _now - _lastTimestamp;
+        if (exp == 0) return Constants.RAY;
+
+        uint256 expMinusOne = exp - 1;
+        uint256 expMinusTwo = exp > 2 ? exp - 2 : 0;
+        uint256 rps = _rate / Constants.SECONDS_PER_YEAR;
+        uint256 rpsSq = (rps * rps) / Constants.RAY;
+
+        uint256 basePowerTwo = (rpsSq * expMinusOne) / 2;
+        uint256 basePowerThree = (basePowerTwo * rps * expMinusTwo) / (Constants.RAY * 3);
+
+        return Constants.RAY + (rps * exp) + basePowerTwo + basePowerThree;
+    }
+
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? a : b;
     }
