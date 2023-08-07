6e4e9c768af99ad8c4bf65984cf19e6d77f2a674
0xKoiner <alexgray.bcn.esp@gmail.com>
2023-08-07 09:30:00 +0200

    perf: optimize rayMul and rayDiv with assembly

--- contracts/library/MathLib.sol
+++ contracts/library/MathLib.sol
@@ -6,12 +6,21 @@ import {Constants} from "../type/Types.sol";
 /// @title MathLib
 /// @notice Fixed-point arithmetic for ray (1e27) and wad (1e18) math
 library MathLib {
-    function rayMul(uint256 a, uint256 b) internal pure returns (uint256) {
-        return (a * b + Constants.HALF_RAY) / Constants.RAY;
+    function rayMul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        assembly {
+            if iszero(or(iszero(b), iszero(gt(a, div(sub(not(0), div(b, 2)), b))))) { revert(0, 0) }
+            c := div(add(mul(a, b), div(b, 2)), 1000000000000000000000000000)
+        }
     }
 
-    function rayDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-        return (a * Constants.RAY + b / 2) / b;
+    function rayDiv(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        assembly {
+            if iszero(b) { revert(0, 0) }
+            if iszero(iszero(gt(a, div(sub(not(0), div(b, 2)), 1000000000000000000000000000)))) {
+                revert(0, 0)
+            }
+            c := div(add(mul(a, 1000000000000000000000000000), div(b, 2)), b)
+        }
     }
 
     function wadMul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -31,8 +40,13 @@ library MathLib {
         uint256 exp = _now - _lastTimestamp;
         if (exp == 0) return Constants.RAY;
 
-        uint256 expMinusOne = exp - 1;
-        uint256 expMinusTwo = exp > 2 ? exp - 2 : 0;
+        uint256 expMinusOne;
+        uint256 expMinusTwo;
+        unchecked {
+            expMinusOne = exp - 1;
+            expMinusTwo = exp > 2 ? exp - 2 : 0;
+        }
+
         uint256 rps = _rate / Constants.SECONDS_PER_YEAR;
         uint256 rpsSq = (rps * rps) / Constants.RAY;
 
