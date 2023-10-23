ca3b0a2d696d76e095c6c7ecf5e158c6474f17d8
0xKoiner <alexgray.bcn.esp@gmail.com>
2023-10-23 10:30:00 +0200

    docs: add NatSpec to type definitions and events

--- contracts/type/Events.sol
+++ contracts/type/Events.sol
@@ -1,10 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+/// @param asset Address of the deposited asset
+/// @param user Address of the depositor
+/// @param amount Amount deposited
 event Deposit(address indexed asset, address indexed user, uint256 amount);
+
+/// @param asset Address of the withdrawn asset
+/// @param user Address of the withdrawer
+/// @param amount Amount withdrawn
 event Withdraw(address indexed asset, address indexed user, uint256 amount);
+
+/// @param asset Address of the borrowed asset
+/// @param user Address of the borrower
+/// @param amount Amount borrowed
 event Borrow(address indexed asset, address indexed user, uint256 amount);
+
+/// @param asset Address of the repaid asset
+/// @param user Address of the repayer
+/// @param amount Amount repaid
 event Repay(address indexed asset, address indexed user, uint256 amount);
+
+/// @param collateralAsset Address of the seized collateral
+/// @param debtAsset Address of the repaid debt asset
+/// @param user Address of the liquidated user
+/// @param debtCovered Amount of debt covered
+/// @param collateralSeized Amount of collateral seized
 event Liquidation(
     address indexed collateralAsset,
     address indexed debtAsset,
@@ -12,7 +33,16 @@ event Liquidation(
     uint256 debtCovered,
     uint256 collateralSeized
 );
+
+/// @param asset Address of the reserve asset
+/// @param mToken Address of the mToken contract
 event ReserveInitialized(address indexed asset, address indexed mToken);
+
+/// @param asset Address of the reserve asset
+/// @param liquidityRate Updated liquidity rate
+/// @param borrowRate Updated borrow rate
+/// @param liquidityIndex Updated liquidity index
+/// @param borrowIndex Updated borrow index
 event InterestUpdated(
     address indexed asset,
     uint256 liquidityRate,
--- contracts/type/Types.sol
+++ contracts/type/Types.sol
@@ -3,31 +3,49 @@ pragma solidity 0.8.19;
 
 /// @notice Reserve configuration and state
 struct ReserveData {
+    /// @dev Normalized income for deposits (RAY)
     uint128 liquidityIndex;
+    /// @dev Normalized variable debt (RAY)
     uint128 borrowIndex;
+    /// @dev Current liquidity rate (RAY)
     uint128 liquidityRate;
+    /// @dev Current borrow rate (RAY)
     uint128 borrowRate;
+    /// @dev Timestamp of last reserve update
     uint40 lastUpdateTimestamp;
+    /// @dev Address of the associated mToken
     address mTokenAddress;
+    /// @dev Reserve factor in basis points
     uint16 reserveFactorBps;
+    /// @dev Liquidation threshold in basis points
     uint16 liquidationThresholdBps;
+    /// @dev Liquidation bonus in basis points
     uint16 liquidationBonusBps;
+    /// @dev Loan-to-value ratio in basis points
     uint16 ltvBps;
+    /// @dev Whether the reserve accepts deposits
     bool isActive;
 }
 
 /// @notice Per-user reserve state
 struct UserReserveData {
+    /// @dev Scaled deposit balance (shares)
     uint256 depositShares;
+    /// @dev Scaled borrow balance (shares)
     uint256 borrowShares;
 }
 
 /// @notice Parameters for initializing a new reserve
 struct ReserveConfig {
+    /// @dev Address of the underlying ERC-20 asset
     address asset;
+    /// @dev Reserve factor in basis points
     uint16 reserveFactorBps;
+    /// @dev Liquidation threshold in basis points
     uint16 liquidationThresholdBps;
+    /// @dev Liquidation bonus in basis points
     uint16 liquidationBonusBps;
+    /// @dev Loan-to-value ratio in basis points
     uint16 ltvBps;
 }
 
